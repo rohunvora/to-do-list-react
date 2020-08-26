9118bb69d55324b0612605a8d2488cfd4ee5d172
Rohun Vora <emailrohun@gmail.com>
2020-08-26 15:07:14 -0700

    [index.js] working placeholder todos

---
+++ src/MyList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+import ListItem from './components/ListItem'
+
+class MyList extends Component {
+  render() {
+    let todoItems = this.props.theList.map((item, index) => (
+      <ListItem doThis={item} key={'todo' + index} />
+    ))
+    return (
+      <div>
+        <h1> Things I should stop procrastinating:</h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default MyList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import MyList from "./MyList";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var toDos = ["Buy ice cream", "Eat the ice cream", "Go to the gym", "Eat Pizza"]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<MyList theList={toDos}/>, document.getElementById("root"))
